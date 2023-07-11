e7c127cb20d5766ad16d895d0b5226da09726acf
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2023-07-11 17:06:14 +0100

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const audioPlayer = {
-    //Put audio files into a map() and use loop to search map
-
     boomSound: new Audio("./sounds/boom.wav"),
     clapSound: new Audio("./sounds/clap.wav"),
     HiHatSound: new Audio("./sounds/hihat.wav"),
@@ -11,30 +9,82 @@ const audioPlayer = {
     tinkSound: new Audio("./sounds/tink.wav"),
     tomSound: new Audio("./sounds/tom.wav"),
 
-    playAudio: () => {
-        let buttonWrapper = document.getElementById("buttonsWrapper");
-        buttonWrapper.addEventListener("click", (event) => {
-            switch (event.target.id) {
-                case "boomSound":
-                    console.log(boomSound);
-
-                    // boomSound.play();
-            }
-            
-        })
+    keyPressedPlayAudio(event) {
+        switch (event.key) {
+            case "q":
+                this.boomSound.play();
+                break;
+            case "w":
+                this.clapSound.play();
+                break;
+            case "e":
+                this.HiHatSound.play();
+                break;
+            case "r":
+                this.kickSound.play();
+                break;
+            case "t":
+                this.openhatSound.play();
+                break;
+            case "u":
+                this.rideSound.play();
+                break;
+            case "i":
+                this.snareSound.play();
+                break;
+            case "o":
+                this.tinkSound.play();
+                break;
+            case "p":
+                this.tomSound.play();
+                break;
+            default:
+                console.log("Error: Event Key Not Found");
+        }
     },
 
+    buttonPressedPlayAudio(event) {
+        console.log(event);
+        switch (event.target.id) {
+            case "buttonBoom":
+                // console.log(this.boomSound);
+                this.boomSound.play();
+                break;
+            case "buttonClap":
+                this.clapSound.play();
+                break;
+            case "buttonHiHat":
+                this.HiHatSound.play();
+                break;
+            case "buttonKick":
+                this.kickSound.play();
+                break;
+            case "buttonOpenHat":
+                this.openhatSound.play();
+                break;
+            case "buttonRide":
+                this.rideSound.play();
+                break;
+            case "buttonSnare":
+                this.snareSound.play();
+                break;
+            case "buttonTink":
+                this.tinkSound.play();
+                break;
+            case "buttonTom":
+                this.tomSound.play();
+                break;
+            default:
+                console.log("Error: Button ID Not Found");
+        }
+    },
 };
 
-
-audioPlayer.playAudio();
-let boomAudio = new Audio("./sounds/boom.wav");
-let boomButton = document.getElementById("boomSound");
-console.log(boomButton);
-
-
-boomButton.addEventListener("click", (event) => {
-    boomAudio.play();
+let buttonWrapper = document.getElementById("buttonsWrapper");
+buttonWrapper.addEventListener("click", (event) => {
+    audioPlayer.buttonPressedPlayAudio(event);
 });
 
-
+document.addEventListener("keypress", (event) => {
+    audioPlayer.keyPressedPlayAudio(event);
+});
